2928957a96843414e1865876c214988a4fd83cd9
eralora <63308218+eralora@users.noreply.github.com>
2020-12-25 19:40:45 +0600

    Adding BallRollling files

---
+++ BallRoller/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    public GameObject highUI;
+    public GameObject currentUI;
+    public GameObject gameOver;
+ 
+    
+    public float time;
+    public void CompleteLevel() 
+    {
+        
+        highUI.SetActive(true);
+        currentUI.SetActive(true);
+        completeLevelUI.SetActive(true);
+
+    }
+    public void EndGame()
+    
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER!");
+            gameOver.SetActive(true);
+        }
+        
+    }
+
+
+    public void Quit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ BallRoller/Assets/Scripts/Result3.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result3 : MonoBehaviour
+{
+
+    
+    public Text textHigh;
+    public Text textCurrent;
+    public EndTrigger endTrigger;
+    public CountdownController countdownController;
+    float time = 0;
+
+    private void Update()
+    {
+        try
+        {
+            if (endTrigger.isFinish)
+            {
+
+                LoadPlayer();
+
+                if (time < countdownController.finishTime)
+                {
+                    SavePlayer();
+                    LoadPlayer();
+                    textHigh.text = time.ToString(".000");
+                    textCurrent.text = countdownController.finishTime.ToString(".000");
+                }
+                else
+                {
+                    textHigh.text = time.ToString(".00");
+                    textCurrent.text = countdownController.finishTime.ToString(".000");
+                }
+
+
+
+            }
+        }
+
+        catch
+        {
+            SavePlayer();
+            LoadPlayer();
+
+            if (time < countdownController.finishTime)
+            {
+                SavePlayer();
+                LoadPlayer();
+                textHigh.text = time.ToString(".000");
+                textCurrent.text = countdownController.finishTime.ToString(".000");
+            }
+            else
+            {
+                textHigh.text = time.ToString(".00");
+                textCurrent.text = countdownController.finishTime.ToString(".000");
+            }
+        }
+    }
+
+    public void SavePlayer()
+    {
+        SystemSave3.SavePlayer(countdownController);
+    }
+
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SystemSave3.LoadPlayer();
+
+        //Debug.Log("This is saved time:" + data.time);
+
+        time = data.time;
+        //Debug.Log("this is assigned time" + time);
+    }
+
+
+}
